ce01ab832d1d4f59db926c39f2e3abe39a1a0757
amyvelasquez <amymontiel@me.com>
2021-03-11 20:45:44 -0500

    edited code

--- src/testTheGIT/GIT.java
+++ src/testTheGIT/GIT.java
@@ -2,4 +2,13 @@ package testTheGIT;
 
 public class GIT {
 
+	public static void main(String[] args) {
+		
+		System.out.println("This is GIT");
+		
+		System.out.println("We're learning GIT");
+		
+		
+	}
+	
 }
